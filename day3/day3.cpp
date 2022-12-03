f1445f161961a592e819a6454b69b58c188d5672
cinaral <alevent.cinar@gmail.com>
2022-12-03 00:48:14 -0600

    Add day3 b

--- day3/day3.cpp
+++ day3/day3.cpp
@@ -91,45 +91,87 @@ main(int argc, char *argv[])
 
 	unsigned total = 0;
 
-	for (size_t i = 0; i < pair_count; ++i) {
-		bool left_freq[item_types];
-		bool right_freq[item_types];
+	for (size_t i = 0; i < pair_count; i += 3) {
+		bool freq[3][item_types];
 
-		for (size_t i = 0; i < item_types; ++i) {
-			left_freq[i] = 0;
-			right_freq[i] = 0;
-		}
-
-		for (size_t j = 0; j < pair_lengths[i]; ++j) {
-			int left = static_cast<size_t>(left_pairs[i][j]);
-
-			if (left - 97 > 0) {
-				left = left - 97;
-			} else {
-				left = left - 65 + 26;
-			}
-			if (!left_freq[left]) {
-				left_freq[left] = true;
-			}
-
-			int right = static_cast<size_t>(right_pairs[i][j]);
-
-			if (right - 97 > 0) {
-				right = right - 97;
-			} else {
-				right = right - 65 + 26;
-			}
-			if (!right_freq[right]) {
-				right_freq[right] = true;
+		for (size_t j = 0; j < 3; ++j) {
+			for (size_t k = 0; k < item_types; ++k) {
+				freq[j][k] = false;
 			}
 		}
 
+		for (size_t j = 0; j < 3; ++j) {
+			for (size_t k = 0; k < pair_lengths[i + j]; ++k) {
+				int left = static_cast<size_t>(left_pairs[i + j][k]);
+
+				if (left - 97 > 0) {
+					left = left - 97;
+				} else {
+					left = left - 65 + 26;
+				}
+				if (!freq[j][left]) {
+					freq[j][left] = true;
+				}
+
+				int right = static_cast<size_t>(right_pairs[i + j][k]);
+
+				if (right - 97 > 0) {
+					right = right - 97;
+				} else {
+					right = right - 65 + 26;
+				}
+				if (!freq[j][right]) {
+					freq[j][right] = true;
+				}
+			}
+		}
+
 		for (size_t i = 0; i < item_types; ++i) {
-			if (left_freq[i] && right_freq[i]) {
+			if (freq[0][i] && freq[1][i] && freq[2][i]) {
 				total += i + 1;
 			}
 		}
 	}
+
+	// for (size_t i = 0; i < pair_count; ++i) {
+	//	bool left_freq[item_types];
+	//	bool right_freq[item_types];
+
+	//	for (size_t i = 0; i < item_types; ++i) {
+	//		left_freq[i] = 0;
+	//		right_freq[i] = 0;
+	//	}
+
+	//	for (size_t j = 0; j < pair_lengths[i]; ++j) {
+	//		int left = static_cast<size_t>(left_pairs[i][j]);
+
+	// if (left - 97 > 0) {
+	//	left = left - 97;
+	// } else {
+	//	left = left - 65 + 26;
+	// }
+	// if (!left_freq[left]) {
+	//	left_freq[left] = true;
+	// }
+
+	// int right = static_cast<size_t>(right_pairs[i][j]);
+
+	// if (right - 97 > 0) {
+	//	right = right - 97;
+	// } else {
+	//	right = right - 65 + 26;
+	// }
+	// if (!right_freq[right]) {
+	//	right_freq[right] = true;
+	// }
+	//	}
+
+	//	for (size_t i = 0; i < item_types; ++i) {
+	//		if (left_freq[i] && right_freq[i]) {
+	//			total += i + 1;
+	//		}
+	//	}
+	//}
 
 	printf("%u total\n", total);
 
